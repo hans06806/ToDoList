2a8b05a3784dd8723400fe5018ac2e71b4690cd1
Hans <hans06806@gmail.com>
2025-03-06 16:45:15 +1300

    Imporve TodoList rendering

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,16 +8,31 @@ function App() {
 
     useEffect(() => {
         const getTodos = async () => {
-            const data = await fetchTodos();
-            setTodos(data);
+            try {
+                const data = await fetchTodos();
+                console.log("Fetched Todos from API:", data); // Debugging log
+                setTodos(data);
+            } catch (error) {
+                console.error('Error fetching todos:', error);
+            }
         };
         getTodos();
     }, []);
 
+    const handleAddTodo = async (task) => {
+        try {
+            const newTodo = await createTodo(task);
+            console.log("New Todo Created:", newTodo); // Debugging log
+            setTodos((prevTodos) => [...prevTodos, newTodo]); // Update state
+        } catch (error) {
+            console.error("Error adding todo:", error);
+        }
+    };
+
     return (
         <div>
             <h1>To-Do List</h1>
-            <TodoForm onAddTodo={createTodo} />
+            <TodoForm onAddTodo={handleAddTodo} />
             <TodoList todos={todos} onUpdateTodo={updateTodo} onDeleteTodo={deleteTodo} />
         </div>
     );
--- frontend/src/TodoList.js
+++ frontend/src/TodoList.js
@@ -2,6 +2,12 @@ import React from 'react';
 import TodoItem from './TodoItem';
 
 const TodoList = ({ todos, onUpdateTodo, onDeleteTodo }) => {
+    console.log("Todos in TodoList:", todos); // Debugging log
+
+    if (!todos.length === 0) {
+        return <p>No tasks to show</p>;
+    }
+
     return (
         <ul>
             {todos.map((todo) => (
